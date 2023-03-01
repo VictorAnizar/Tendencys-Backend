b52bb2b7915e9912f9b0f3e6f2f787cc8a839eff
programador-dss7 <programador-dss7@funcionpublica.gob.mx>
2023-02-28 23:34:49 -0600

    modificando archivo app.js para que pueda correr server

--- app.js
+++ app.js
@@ -13,4 +13,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', require('./routes/main.routes'));
 
+// Se indican qué respuestas del backend pueden ser compartidas a alguna peticion de un determinado origen
+app.use(
+    function (req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+        next();
+        
+    }
+)
+
+//Configuración de la BD
+const mongoose = require('mongoose');
+
+// Conectarse a la base de datos mediante la URI
+// mongoose.connect(process.env.MONGO_URI);
+
+// Entrar en modo depurador en mongoose
+mongoose.set("debug", true);
+
+// usamos los modelos Aplications, Authorizations y Logs
+
+
+//  activar servidor backend en el puerto definido
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server listening on port ${process.env.PORT || 3000}!`);
+});
+
+
 module.exports = app;
